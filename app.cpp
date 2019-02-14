38298dc13f16c59238f27c691bb455c97d6a912c
luddite478 <luddite478@gmail.com>
2019-02-14 23:12:38 +0300

    benchmark

--- app.cpp
+++ app.cpp
@@ -5,11 +5,22 @@
 #include <iostream>
 #include <string>
 
+// #define BENCH
 #define W 1024
 #define H 576
 
-// void apply_blue_edgess(cv::Mat& matrix);
-// void apply_blue_edgess(cv::Mat& matrix, int mask_h, int mask_w);
+#ifdef BENCH
+  #include <sys/time.h>
+  typedef unsigned long long timestamp_t;
+
+  static timestamp_t
+  get_timestamp ()
+  {
+    struct timeval now;
+    gettimeofday (&now, NULL);
+    return  now.tv_usec + (timestamp_t)now.tv_sec * 1000000;
+  }
+#endif
 
 void createMask(cv::Mat& matrix, cv::Mat& mask, cv::Mat& inverted_mask) {
   int mask_h = H * 50/100;
@@ -25,42 +36,51 @@ void createMask(cv::Mat& matrix, cv::Mat& mask, cv::Mat& inverted_mask) {
 }
 
 
-struct blue_edges_filter {
-  blue_edges_filter(int width, int height)
-  : gray_image(width, height, CV_8UC1),
-    blured(width, height, CV_8UC1),
-    canny(width, height, CV_8UC1),
-    canny_3d(width, height, CV_8UC3),
-    in_range_mask(width, height, CV_8UC3)
-  {  }
+// struct blue_edges_filter {
+//   blue_edges_filter(int width, int height)
+//   : gray_image(width, height, CV_8UC1),
+//     blured(width, height, CV_8UC1),
+//     canny(width, height, CV_8UC1),
+//     canny_3d(width, height, CV_8UC3),
+//     in_range_mask(width, height, CV_8UC3)
+//   {  }
+//
+//   int operator()(cv::Mat& matrix, cv::Mat& mask, cv::Mat& inverted_mask) {
+//
+//     cv::cuda::bitwise_and (matrix, mask, internal_mask_matrix);
+//     cv::cuda::bitwise_and (matrix, inverted_mask, external_mask_matrix);
+//
+//     cv::cuda::cvtColor( matrix, gray_image, CV_BGR2GRAY );
+//     cv::cuda::GaussianBlur( gray_image, blured, cv::Size( 5, 5 ), 0, 0 );
+//     cv::cuda::Canny(blured, canny, 0, 100);
+//     cv::cuda::cvtColor( canny, canny_3d, CV_GRAY2BGR );
+//     cv::inRange(canny_3d, cv::Scalar(255,255,255), cv::Scalar(255,255,255), in_range_mask);
+//     canny_3d.setTo(cv::Scalar(0, 171, 255), in_range_mask);
+//     cv::GaussianBlur( canny_3d, matrix, cv::Size( 5, 5 ), 0, 0 );
+//     cv::bitwise_and(matrix, mask, matrix);
+//
+//     cv::cvtColor( external_mask_matrix, external_mask_matrix, CV_BGR2XYZ );
+//
+//     cv::add(matrix, external_mask_matrix, matrix);
+//   }
+//
+//   private:
+//     cv::gpu::GpuMat gray_image, blured, canny, canny_3d, in_range_mask;
+//     cv::Mat external_mask_matrix, internal_mask_matrix;
+// };
 
-  int operator()(cv::Mat& matrix, cv::Mat& mask, cv::Mat& inverted_mask) {
+void black_and_white_filter(cv::Mat& matrix) {
 
-    cv::bitwise_and(matrix, mask, internal_mask_matrix);
-    cv::bitwise_and(matrix, inverted_mask, external_mask_matrix);
-
-    cv::cvtColor( matrix, gray_image, CV_BGR2GRAY );
-    cv::GaussianBlur( gray_image, blured, cv::Size( 5, 5 ), 0, 0 );
-    cv::Canny(blured, canny, 0, 100);
-    cv::cvtColor( canny, canny_3d, CV_GRAY2BGR );
-    cv::inRange(canny_3d, cv::Scalar(255,255,255), cv::Scalar(255,255,255), in_range_mask);
-    canny_3d.setTo(cv::Scalar(0, 171, 255), in_range_mask);
-    cv::GaussianBlur( canny_3d, matrix, cv::Size( 5, 5 ), 0, 0 );
-    cv::bitwise_and(matrix, mask, matrix);
-
-    cv::cvtColor( external_mask_matrix, external_mask_matrix, CV_BGR2XYZ );
-
-    cv::add(matrix, external_mask_matrix, matrix);
-  }
-
-  private:
-    cv::Mat gray_image, blured, canny, canny_3d, in_range_mask;
-    cv::Mat external_mask_matrix, internal_mask_matrix;
-};
-
+  cv::cvtColor( matrix, matrix, CV_BGR2GRAY );
+  cv::cvtColor( matrix, matrix, CV_GRAY2BGR );
+}
 
 int main(int argc, char *argv[])
 {
+    #ifdef BENCH
+      timestamp_t t0 = get_timestamp();
+    #endif
+
     if(argc < 5) return 1;
 
     std::string input_filename = argv[1]; //cut.mp4
@@ -95,7 +115,7 @@ int main(int argc, char *argv[])
     cv::Mat inverted_mask;
     int count;
 
-    blue_edges_filter apply_blue_edgess(std::stoi(width), std::stoi(height));
+    // blue_edges_filter apply_blue_edgess(std::stoi(width), std::stoi(height));
 
     while(1)
     {
@@ -106,12 +126,14 @@ int main(int argc, char *argv[])
 
         cv::Mat matrix( H, W, CV_8UC3, bitmap );
 
-        if (mask.empty() == 1){
-          createMask(matrix, mask, inverted_mask);
-        }
+        // if (mask.empty() == 1){
+        //   createMask(matrix, mask, inverted_mask);
+        // }
 
-        apply_blue_edgess(matrix, mask, inverted_mask);
-    
+        // apply_blue_edgess(matrix, mask, inverted_mask);
+        // std::cout << "!23123" << std::endl;
+        black_and_white_filter(matrix);
+
         unsigned char* processed_raw_frame = ( unsigned char* )( matrix.data );
         fwrite(processed_raw_frame, 1, H*W*3, pipeout);
     }
@@ -120,6 +142,12 @@ int main(int argc, char *argv[])
     pclose(pipein);
     fflush(pipeout);
     pclose(pipeout);
+
+    #ifdef BENCH
+      timestamp_t t1 = get_timestamp();
+      double secs = (t1 - t0) / 1000000.0L;
+      std::cout << secs << std::endl;
+    #endif
 
     return 0;
 }
